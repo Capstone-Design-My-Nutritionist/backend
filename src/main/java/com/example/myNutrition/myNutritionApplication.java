bf94b57f0ab3551d28a74817158d72cb268fb043
gimgisu <gisu1102@gmail.com>
2025-03-12 18:13:33 +0900

    First Commit

---
+++ src/main/java/com/example/myNutrition/myNutritionApplication.java
@@ -0,0 +1,13 @@
+package com.example.myNutrition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class myNutritionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(myNutritionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myNutrition/MyNutritionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myNutrition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNutritionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
