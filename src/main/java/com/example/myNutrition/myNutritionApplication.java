c791a6a3034f91f752154dd3edd6e66bd3857633
gimgisu <gisu1102@gmail.com>
2025-04-04 01:32:54 +0900

    feat :: Swagger 필터 제외 application 에 추가

--- src/main/java/com/example/myNutrition/myNutritionApplication.java
+++ src/main/java/com/example/myNutrition/myNutritionApplication.java
@@ -1,9 +1,12 @@
 package com.example.myNutrition;
 
+import com.example.myNutrition.common.dto.PermitAllProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(PermitAllProperties.class)
 public class myNutritionApplication {
 
 	public static void main(String[] args) {
