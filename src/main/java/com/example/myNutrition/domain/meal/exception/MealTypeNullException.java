9e37f50841d8eebd056611ea2402eb6f36c2a57d
gimgisu <gisu1102@gmail.com>
2025-05-19 22:18:25 +0900

    refactor : mealType Null 처리 추가

---
+++ src/main/java/com/example/myNutrition/domain/meal/exception/MealTypeNullException.java
@@ -0,0 +1,7 @@
+package com.example.myNutrition.domain.meal.exception;
+
+public class MealTypeNullException extends RuntimeException {
+    public MealTypeNullException(String message) {
+        super(message);
+    }
+}
